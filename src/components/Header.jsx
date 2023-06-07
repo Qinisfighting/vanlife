fd6970f552f718b97443c795d06384f028eb7b92
Qinisfighting <yq.qualmann@gmail.com>
2023-06-07 10:36:53 +0200

    use NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,25 @@ import { Link, NavLink } from "react-router-dom"
 import avatar from '../assets/avatar.png'
 
 export default function Header() {
+    const activeStyles = {
+        color: '#2c5e75e1',
+        textDecoration: 'underline #2c5e75e1 1.5px'
+    }
+
     return (
         <header>
             <Link to='/' className="site-logo">#VANLIFE</Link>
             <nav>
-                <NavLink to='host'>
+                <NavLink to='host' style={({isActive}) => isActive ? activeStyles : null}>
                    Host
                 </NavLink>
-                <NavLink to='about'>
+                <NavLink to='about' style={({isActive}) => isActive ? activeStyles : null}>
                    About
                 </NavLink>
-                <NavLink to='vans'>
+                <NavLink to='vans' style={({isActive}) => isActive ? activeStyles : null}>
                    Vans
                 </NavLink>
-                <NavLink to='login' className="login-link">
+                <NavLink to='login' className="login-link" style={({isActive}) => isActive ? activeStyles : null}>
                    <img 
                         src={avatar} 
                         className="login-icon"
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -3,19 +3,26 @@ import { Outlet } from "react-router-dom"
 
 
 export default function HostLayout() {
+
+    const activeStyles = {
+        color: 'rgb(77, 76, 76)',
+        fontWeight: 600,
+        textDecoration: 'underline'
+    }
+
     return (
        <div className="host-container">
          <nav className="host-nav">     
-           <NavLink to='/host'>
+           <NavLink to='.' style={({isActive}) => isActive ? activeStyles : null}>
               Dashboard
            </NavLink>
-         <NavLink to='/host/income'>
+         <NavLink to='income' style={({isActive}) => isActive ? activeStyles : null}>
               Income
          </NavLink>
-         <NavLink to='/host/hostvans'>
+         <NavLink to='hostvans' style={({isActive}) => isActive ? activeStyles : null}>
               Vans
          </NavLink>
-         <NavLink to='/host/reviews'>
+         <NavLink to='reviews' style={({isActive}) => isActive ? activeStyles : null}>
               Reviews
          </NavLink>       
        </nav>
