afd48dc068da973bde78892166594fd16a6cf124
Qinisfighting <yq.qualmann@gmail.com>
2023-06-18 10:46:10 +0200

    review page done

--- src/pages/Host/Income.jsx
+++ src/pages/Host/Income.jsx
@@ -1,15 +1,14 @@
-
-
+import income_graph from "../../assets/income_graph.png"
 
 export default function Income() {
     return (
-        <div className="income-container">
+        <div className="nexted-container">
            <h2>Income</h2>
            <p className="transaction-time">Last <span className="span1">30 days</span></p>
            <h1>€2,260</h1>
-           <img alt="income_graph" src="../src/assets/income_graph.png" />
+           <img alt="income_graph" src={income_graph} />
            <div className="transaction">
-               <p className="transaction-title">Your transactions(3) <span className="span2">30 days</span></p>
+               <p className="transaction-title">Your transactions(3) <span className="span2">Last 30 days</span></p>
                <hr />
                <div className="transaction-list">
                  <p className="transaction-nr">€720 <span className="span2">1/12/22</span></p>
--- src/pages/Host/Reviews.jsx
+++ src/pages/Host/Reviews.jsx
@@ -1,7 +1,45 @@
-import { Link } from "react-router-dom"
+import reviews_graph from "../../assets/reviews_graph.png"
+import star from "../../assets/star.png"
 
 export default function Review() {
+    const reviewsData = [
+        {
+            rating: 5,
+            name: "Elliot",
+            date: "January 3, 2023",
+            text: "The beach bum is such an awesome van! Such a comfortable trip. We had it for 2 weeks and there was not a single issue. Super clean when we picked it up and the host is very comfortable and understanding. Highly recommend!",
+            id: "1",
+        },
+        {
+            rating: 5,
+            name: "Sandy",
+            date: "December 12, 2022",
+            text: "This is our third time using the Modest Explorer for our travels and we love it! No complaints, absolutely perfect!",
+            id: "2",
+        },
+    ]
+    const renderReviewData = () => {
+       return reviewsData.map(review =>( 
+        <>
+        <div className="transaction" key={review.id}>
+           {[...Array(review.rating)].map((_, i) => (
+                            <img alt="star" key={i} src={star} className="star" style={{width:20, paddingRight:5 }} />
+           ))}
+          <h4>{review.name} <span className="span3">{review.date}</span></h4> 
+          <p className="review-text">{review.text}</p>
+          <hr />
+        </div>
+        </>
+        ))
+
+    }
+
     return (
-        <h1>Host Review here</h1>
+        <div className="nexted-container">
+           <h2>Your reviews<span className="span2"> last <u>30 days</u></span></h2>
+           <img alt="reviews_graph" src={reviews_graph} />
+           <h3 className="transaction-title">Reviews({reviewsData.length})</h3> 
+           {renderReviewData()}
+        </div>
     )
 }
