e00d3e8dd7b42eb7d53967e810d8914cf191f8c6
Qinisfighting <yq.qualmann@gmail.com>
2023-06-18 08:06:35 +0200

    finish income page

--- src/pages/Host/Income.jsx
+++ src/pages/Host/Income.jsx
@@ -1,7 +1,22 @@
-import { Link } from "react-router-dom"
+
+
 
 export default function Income() {
     return (
-        <h1>Host Income here</h1>
+        <div className="income-container">
+           <h2>Income</h2>
+           <p className="transaction-time">Last <span className="span1">30 days</span></p>
+           <h1>€2,260</h1>
+           <img alt="income_graph" src="../src/assets/income_graph.png" />
+           <div className="transaction">
+               <p className="transaction-title">Your transactions(3) <span className="span2">30 days</span></p>
+               <hr />
+               <div className="transaction-list">
+                 <p className="transaction-nr">€720 <span className="span2">1/12/22</span></p>
+                 <p className="transaction-nr">€560 <span className="span2">10/11/22</span></p>
+                 <p className="transaction-nr">€980 <span className="span2">12/11/22</span></p>
+               </div>
+           </div>
+        </div>
     )
 }
